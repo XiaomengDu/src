94fe9a0bcd8561f8704ccf434db0d215b02f8140
XiaomengDu <126791867+XiaomengDu@users.noreply.github.com>
2024-02-07 00:55:11 +0200

    ex7

---
+++ xiaomengdu_tasks/crud/db.php
@@ -0,0 +1,16 @@
+<?php
+$severname = "web-dev-env-main-db-1";
+$username = "app1";
+$password = "password";
+$dbname = "app1";
+
+
+//create database connection
+$conn = new mysqli($severname,$username,$password,$dbname);
+
+
+ //check connection
+ if ($conn->connect_error){
+    die("Connection Failed:" . $conn->connect_error);
+ }
+?>
